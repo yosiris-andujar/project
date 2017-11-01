441b7e06b66b3c3ead2fc24bd93563563d55ad98
Alexie Ortiz Falu <alexie_15@hotmail.com>
2017-11-01 10:00:47 -0400

    Add project files.

---
+++ Test_1/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Test_1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            // Remove formating - xml
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            // Indentin the type of data
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            // Using camel case instead of proper case
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ Test_1/Controllers/PatientsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Test_1;
+
+namespace Test_1.Controllers
+{
+    public class PatientsController : ApiController
+    {
+        private DocSchedulerEntities db = new DocSchedulerEntities();
+
+        // GET: api/Patients
+        public IEnumerable<Patient> GetPatients()
+        {
+            
+            List<Patient> patients_list = new List<Patient>();
+            foreach (var item in db.Patients)
+            {
+                Patient patient2 = new Patient();
+                patient2.patientId = item.patientId;
+                patient2.patientEmail = item.patientEmail;
+                patient2.patientPhone = item.patientPhone;
+                patient2.lastName = item.lastName;
+                patient2.firstName = item.firstName;
+
+                patients_list.Add(patient2);
+            }
+            return patients_list;
+        }
+
+        // GET: api/Patients/5
+        [ResponseType(typeof(Patient))]
+        public IHttpActionResult GetPatient(long id)
+        {
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(patient);
+        }
+
+        // PUT: api/Patients/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPatient(long id, Patient patient)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != patient.patientId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(patient).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PatientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Patients
+        [ResponseType(typeof(Patient))]
+        public IHttpActionResult PostPatient(Patient patient)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Patients.Add(patient);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = patient.patientId }, patient);
+        }
+
+        // DELETE: api/Patients/5
+        [ResponseType(typeof(Patient))]
+        public IHttpActionResult DeletePatient(long id)
+        {
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            db.Patients.Remove(patient);
+            db.SaveChanges();
+
+            return Ok(patient);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PatientExists(long id)
+        {
+            return db.Patients.Count(e => e.patientId == id) > 0;
+        }
+    }
+}
